4779ef460f5186b040048b4f4ad7daf8dd1b408c
charles <charles.delmaire@gmail.com>
2014-04-15 23:00:13 -0400

    ajout google analystic

--- QingPool/src/net/charlesdelmaire/qingpool/accueilActivity.java
+++ QingPool/src/net/charlesdelmaire/qingpool/accueilActivity.java
@@ -1,10 +1,17 @@
 package net.charlesdelmaire.qingpool;
 
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.Fields;
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.Tracker;
 
 public class accueilActivity extends Activity implements OnClickListener {
 	@Override
@@ -14,6 +21,15 @@ public class accueilActivity extends Activity implements OnClickListener {
 
 		View btnClick = findViewById(R.id.btnConnexion);
 		btnClick.setOnClickListener(this);
+
+		Tracker tracker = GoogleAnalytics.getInstance(this).getTracker(
+				"UA-50075921-1");
+
+		HashMap<String, String> hitParameters = new HashMap<String, String>();
+		hitParameters.put(Fields.HIT_TYPE, "appview");
+		hitParameters.put(Fields.SCREEN_NAME, "Home Screen");
+
+		tracker.send(hitParameters);
 	}
 
 	@Override
@@ -26,5 +42,19 @@ public class accueilActivity extends Activity implements OnClickListener {
 			this.startActivity(intent);
 		}
 
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		// The rest of your onStart() code.
+		EasyTracker.getInstance(this).activityStart(this); // Add this method.
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		// The rest of your onStop() code.
+		EasyTracker.getInstance(this).activityStop(this); // Add this method.
 	}
 }
